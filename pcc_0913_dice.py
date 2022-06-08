faa9e06ce8da56edf88e3703e010a87bb5e85e8a
ceeplusharp <ceeplusharp@gmail.com>
2022-06-08 21:30:02 +0800

    Added 10 and 20-sided die

--- pcc_0913_dice.py
+++ pcc_0913_dice.py
@@ -12,10 +12,36 @@ class Dice:
 
     def roll_die(self):
         """Prints a random number between 1 and the chosen number of sides."""
-        print(f"The rolled number is: {randint(1,self.sides)}")
+        print(f"The rolled number from the {self.sides}-sided die is: {randint(1,self.sides)}")
 
-
+# 6-sided die
 roll = Dice()
+roll.roll_die()  # roll 1
+roll.roll_die()  # roll 2
+roll.roll_die()  # roll 3
+roll.roll_die()  # roll 4
+roll.roll_die()  # roll 5
+roll.roll_die()  # roll 6
+roll.roll_die()  # roll 7
+roll.roll_die()  # roll 8
+roll.roll_die()  # roll 9
+roll.roll_die()  # roll 10
+
+# 10-sided die
+roll = Dice(10)
+roll.roll_die()  # roll 1
+roll.roll_die()  # roll 2
+roll.roll_die()  # roll 3
+roll.roll_die()  # roll 4
+roll.roll_die()  # roll 5
+roll.roll_die()  # roll 6
+roll.roll_die()  # roll 7
+roll.roll_die()  # roll 8
+roll.roll_die()  # roll 9
+roll.roll_die()  # roll 10
+
+# 20-sided die
+roll = Dice(20)
 roll.roll_die()  # roll 1
 roll.roll_die()  # roll 2
 roll.roll_die()  # roll 3
