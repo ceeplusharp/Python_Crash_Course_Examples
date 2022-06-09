f53124939acfab4bae6468c78eb6d8504043b804
ceeplusharp <ceeplusharp@gmail.com>
2022-06-09 21:53:08 +0800

    Initial commit

---
+++ pcc_1000_pi_string.py
@@ -0,0 +1,27 @@
+# Chapter 10: Working with a File's Contents.
+
+filename = 'pcc_pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string)
+print(len(pi_string))
+
+# Large Files
+
+filename = 'pcc_pi_million_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
