f96c40735103025f253b49ed9c856fe7c9a5cce9
ceeplusharp <ceeplusharp@gmail.com>
2022-06-12 20:24:08 +0800

    Initial commit

---
+++ pcc_1000_word_count.py
@@ -0,0 +1,19 @@
+# Chapter 10: Working with Multiple Files
+
+def count_words(filename):
+    """Count the approximate number of words in a file."""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the filename '{filename}' does not exist.")
+    else:
+        # Count the approximate number of words in the file.
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file '{filename}' has about {num_words} words.")
+
+
+filenames = ['pcc_alice.txt', 'pcc_siddhartha.txt', 'pcc_moby_dick.txt', 'pcc_little_women.txt']
+for filename in filenames:
+    count_words(filename)
