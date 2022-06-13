0f4f5b2c4c0a921de5daa97c870fad79e7d846b1
ceeplusharp <ceeplusharp@gmail.com>
2022-06-13 22:23:19 +0800

    Modified functions

--- pcc_1000_remember_me.py
+++ pcc_1000_remember_me.py
@@ -1,19 +1,37 @@
-# Chapter 10: Saving and Reading User-Generated Data
+# Chapter 10: Refactoring
 
 import json
 
-# Load the username, if it has been stored previously.
-# Otherwise, prompt for the username and store it.
 
-filename = 'pcc_username.json'
+def get_stored_username():
+    """Greet stored username if available."""
+    filename = 'pcc_username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+
+def get_new_username():
+    """Prompt for a new username."""
     username = input("What is your name? ")
+    filename = 'pcc_username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
         print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+
+
+greet_user()
