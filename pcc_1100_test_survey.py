c4c4f9bb31c22dfa9bae8534ff18f4f1b2714845
ceeplusharp <ceeplusharp@gmail.com>
2022-06-16 21:29:43 +0800

    Revised test case for survey using setup

--- pcc_1100_test_survey.py
+++ pcc_1100_test_survey.py
@@ -1,5 +1,7 @@
 # Chapter 11: Testing the Class
 
+# Chapter 11: Testing the Class
+
 import unittest
 from pcc_1100_survey import AnonymousSurvey
 
@@ -7,23 +9,23 @@ from pcc_1100_survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for the class Anonymous Survey."""
 
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods."""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
     def test_store_single_response(self):
         """Test that a single response is stored properly."""
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_responses('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_responses(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_three_responses(self):
         """Test that three individual responses are stored properly."""
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin']
-        for response in responses:
-            my_survey.store_responses(response)
-
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_responses(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 
 if __name__ == '__main__':
