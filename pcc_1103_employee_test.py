63d9a9ee3b31551fc74eaf1d222eaa0ee28469b3
ceeplusharp <ceeplusharp@gmail.com>
2022-06-17 18:20:32 +0800

    Initial commit

---
+++ pcc_1103_employee_test.py
@@ -0,0 +1,26 @@
+# Chapter 11: Try It Yourself. 11-03: Employee.
+
+
+import unittest
+from pcc_1103_employee_class import Employee
+
+
+class TestEmployeeRaise(unittest.TestCase):
+    """Tests for the class Employee."""
+
+    def setUp(self):
+        """Creates the employee profile."""
+        self.employee1 = Employee('Bojack', 'Horseman', 60_000)
+        self.employee2 = Employee('Dianne', 'Nguyen', 50_000, 10_000)
+
+    def test_give_default_raise(self):
+        """Test the given default raise."""
+        self.assertEqual(self.employee1.give_raise(), 65_000)
+
+    def test_give_custom_raise(self):
+        """Test the given default raise."""
+        self.assertEqual(self.employee2.give_raise(), 60_000)
+
+
+if __name__ == '__main__':
+    unittest.main()
